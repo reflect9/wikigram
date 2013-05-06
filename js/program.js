79384535ef07efb24eb33fe4b42af1d2b749f6b3
TAK YEON LEE <takyeonlee@TAKs-MacBook-Air.local>
2013-05-06 01:50:20 -0400

    after adding langauge and sample

--- js/program.js
+++ js/program.js
@@ -19,7 +19,9 @@ wg.Program = function() {
 	this.reset = function() {
 	};
 	this.setVariable = function(pos,value) {
-		this.sheets[pos.s].columns[pos.c].row[pos.r] = value;
+		var column = this.sheets[pos.s].columns[pos.c];
+		column.row[pos.r] = value;
+		column.inferAll();	// updates in/outbound operation candidates of the column 
 	};
 	this.getVariable = function(pos) {
 		return this.sheets[pos.s].columns[pos.c].row[pos.r];
@@ -28,10 +30,13 @@ wg.Program = function() {
 		this.sheets[pos.s].columns[pos.c].args = value;
 	};
 	this.getArgument = function(pos) {
-		return this.sheets[pos.s].columns[pos.c].args;
+		return (pos)? this.sheets[pos.s].columns[pos.c].args: null;
 	};
 	this.getColumn = function(pos) {
-		return this.sheets[pos.s].columns[pos.c];
+		return (pos)? this.sheets[pos.s].columns[pos.c]: null;
+	};
+	this.getRow = function(pos) {
+		return (pos)? $(this.sheets[pos.s].columns[pos.c].row).trimArray() : [];
 	};
 	this.clearCandidate = function() {
 		_.each(this.sheets, function(sh) {
@@ -39,6 +44,34 @@ wg.Program = function() {
 				col.creatorSignature=null;
 			});
 		});
+	};
+	// pos and row for all columns
+	// for generator to create input candidates
+	this.getAllRows = function() {
+		var allData = _.map(this.sheets, function(s,si) {
+			var a = _.map(s.columns, function(c,ci) {
+				var validRows = $(c.row).trimArray();
+				if(validRows==[]) return null;
+				else return {pos:{s:si,c:ci,r:0},row:validRows};
+			});
+			return a;
+		});
+		return _.filter(_.flatten(allData),function(obj){return obj.row.length>0;});
+	};
+	// returns a list of operations from all the columns 
+	this.getOperations = function(pos) {
+		pos= (pos)? pos:{s:0,r:0,c:0};
+		var nonEmptyColumns = _.filter(this.sheets[pos.s].columns, function(col,i) {
+			return i>0 && col.operation.description!==".";
+		});
+		var opList = _.map(nonEmptyColumns, function(col) {
+			return col.operation;
+		});
+		return opList;
+	};
+	// creates multiple columns from the operation list and insert them at the pos
+	this.insertOperations = function(pos,opList) {
+		this.sheets[pos.s].insertColumnsFromOperations(pos.c+1,opList);
 	};
 	/*
 	 * create an empty sheet at the bottom
