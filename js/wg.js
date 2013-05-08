e9edf17526c4772411e3315bb31b99deef2a7e8e
TAK YEON LEE <takyeonlee@TAKs-MacBook-Air.local>
2013-05-08 17:17:58 -0400

    before rebuilding everything

--- js/wg.js
+++ js/wg.js
@@ -11,27 +11,35 @@ var wg = {
 	selectionStatus: false,
 	selectionBox: null,
 	generator:null,
+	language:null,
+	sample:null,
 	program: null,
+	tab: null,
 	loaders: [],
 	/*
 	 *  Initialize wikigram worksheet
+			prog : initial content of worksheet operations. will be used when loading predefined or existing program 
+			tabInfo :	widget UI and how the progrm would work depends on whether the tab is 'master' or 'child'
+						'master' tabs can have something like run/save functionalities.
+						'child' tabs will have 'save&close' to return the procedure back to it's master tab.
 	 */
-	init: function(prog) {
+	init: function(prog, tabInfo) {
 		// initialize program
 		console.log("wg created");
+		// initialize generator
+		//console.log("generator created");
+		this.generator = new wg.Generator();
 		if(prog) {
 			this.program = prog;
 		} else {
 			this.program = new this.Program();
 			this.program.init();
 		}
-		console.log("wg program initiated");
+		this.tab = tabInfo;
+		//console.log("wg program initiated");
 		this.program.setVariable({s:0,c:0,r:0},$("html")[0]);
-		// initialize generator
-		console.log("generator created");
-		this.generator = new wg.Generator();
 		// initialize widget UI
-		console.log("widget initiated");
+		//console.log("widget initiated");
 		this.widget.init(this.program.sheets);
 	},
 	/*
@@ -58,6 +66,15 @@ var wg = {
 		this.status = false;
 		this.widget.close();
 		this.disableSelection();
+	},
+	/*	
+		send current operations to the master worksheet
+	*/
+	save: function() {
+		var opList = wg.program.getOperations({s:0, c:0, r:0});
+		if(opList && opList!==[] && wg.tab.from && wg.tab.targetColumnPosition) {
+			returnSubProcedure(opList,wg.tab.from,wg.tab.targetColumnPosition);
+		}
 	},
 	toString: function() {
 		return this.program.toString();
